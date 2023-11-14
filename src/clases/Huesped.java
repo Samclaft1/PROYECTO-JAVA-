ce9fe914811b22f1d703ecc6e2067b1c3b6b8507
scamp <scamp@LAPTOP-2R41O8AV>
2023-11-13 23:39:07 -0300

    j

--- src/clases/Huesped.java
+++ src/clases/Huesped.java
@@ -5,60 +5,6 @@ package clases;
  *
  * @author scamp
  */
-public class Huesped extends Reserva {
-    private String nombre;
-    private String apellido;
-    private String telefono;
-
-    //CONSTRUCTORES
-    public Huesped() {
-    }
-
-    public Huesped(String nombre, String apellido, String telefono, String idreserva, String checkin, String checkout, String precio, boolean desayuno, boolean vip, int nrocamas) {
-        super(idreserva, checkin, checkout, precio, desayuno, vip, nrocamas);
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-    }
-    
-    //GETTER Y SETTERS
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    
-    //TO STRING
-
-    @Override
-    public String toString() {
-        return "Huesped{" + "nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + '}';
-    }
-    
-    
-     
-   
-    
-    
-    
+public class Huesped {
     
 }
